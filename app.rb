7f676964662aa0e270dd9a8aa471ee3c011e8036
AlbaMoriWallin123 <94969096+AlbaMoriWallin123@users.noreply.github.com>
2023-02-21 09:36:25 +0100

    v.7 lov

--- app.rb
+++ app.rb
@@ -18,14 +18,14 @@ end
 
 
 get('/') do
+    session[:id] = nil
     slim(:login)
 end
 
 get('/friends') do
     db = SQLite3::Database.new('db/legofigure.db')
     db.results_as_hash = true
-    @name = db.execute("SELECT username FROM user")
-    p @name
+    @name = db.execute("SELECT username FROM user ORDER BY user_id")
     @parts = db.execute("SELECT * FROM partofig")  
     slim(:index)
 end
@@ -37,13 +37,17 @@ post('/new_user') do
     password2 = params[:password2]
     
 
-    if (password == password2)
+    if username == "" || password == "" || password2 == ""
+        session[:error] = "Fyll i alla rutor!!!"
+        redirect('/register')
+    
+    elsif (password == password2)
         db = SQLite3::Database.new('db/legofigure.db')
         db.results_as_hash = true
         username_db = db.execute("SELECT username FROM user") 
         
         username_db.each do |username_db|
-            if username == username_db['username']
+            if username == username_db['username'] 
                 session[:error] = "Användarnamnet är upptaget, välj ett annat"
                 redirect('/register')
             end
@@ -52,6 +56,9 @@ post('/new_user') do
         password_digest = BCrypt::Password.create(password)
         db = SQLite3::Database.new('db/legofigure.db')
         db.execute("INSERT INTO user (username,pwdigest) VALUES (?,?)",username,password_digest)
+        id = db.execute("SELECT user_id FROM user WHERE username = ?", username)
+        db.execute("INSERT INTO partofig (user_id) VALUES (?)",id)
+        
         session[:error] = "Logga in med ditt nya konto"
         redirect('/')
     
@@ -86,9 +93,8 @@ end
 get('/figure') do #ser id:t
     id = session[:id].to_i
     db = SQLite3::Database.new('db/legofigure.db')
-    db.results_as_hash = true
-    @result = db.execute("SELECT * FROM partofig WHERE user_id = ?", id)
-    p "herghgrjhgrhioefjiiho#{@result}"
+    @my_parts = db.execute("SELECT * FROM partofig WHERE user_id = ?", id)
+    @my_name = db.execute("SELECT username FROM users WHERE user_id = ?", id)
     slim(:show)
 end
 
